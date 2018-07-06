c78e6543d8536584140acda37efd550f2fbaace4
Mohd Anuar Mohd Salleh <emnu02@gmail.com>
2018-07-06 08:58:06 +0800

    Update example.php

--- example.php
+++ example.php
@@ -3,6 +3,6 @@
 <?php
 include_once('libs/include.php');
 
-$resPerson = $Model->Person->find(array('id'=>123, array('order'=>'id DESC', 'limit'=>100, 'fields'=>array('colum1', 'column2')));
+$resPerson = $Model->Person->find(array('id'=>123), array('order'=>'id DESC', 'limit'=>100, 'fields'=>array('colum1', 'column2')));
 $person = $resPerson->getArray();
 pr($person);
